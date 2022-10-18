6cc96e3fd0ca5213ba6d2b4ee29730b4d7164138
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-10-18 13:55:03 +0600

    devshunt-frontend-setup-done

---
+++ resources/views/courses/show.blade.php
@@ -0,0 +1,1 @@
+<h2>Courses Show</h2>
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\CourseController;
+use App\Http\Controllers\Frontend\HomePageController;
+
+Route::get('/', [HomePageController::class, 'homePage'])->name('index');
+Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
