d61c46cfe456e8b250481e62f6811571ae2b47ba
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-11-21 22:41:47 +0600

    laravel-yajra-jquery-datatable-setup-done

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\DashboardController;
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/categories', [DashboardController::class, 'categories'])->name('categories.index');
+Route::resource('categories', CategoryController::class);
