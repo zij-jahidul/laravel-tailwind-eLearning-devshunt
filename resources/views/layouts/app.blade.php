f976313d1f460de622924122195f324dbd594eae
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2022-11-21 07:00:36 +0600

    laravel-tailwind-flow-bite-setup-done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @include('layouts.partials.styles')
+    @include('layouts.partials.scripts')
 </head>
 
 <body class="font-sans antialiased">
