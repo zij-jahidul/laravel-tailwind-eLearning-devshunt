67e544f0c5f110a1c6f0c128fdd45f1f3f857c60
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-01-28 22:48:44 +0600

    Page Repository Pattern CRUD with Yajra datatables

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; {{ config('app.name') }} 2022</span>
+            <span>Copyright &copy; {{ config('app.name') }} 2023</span>
         </div>
     </div>
 </footer>
--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\PageController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\DashboardController;
 
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('categories', CategoryController::class);
+Route::resource('pages', PageController::class);
